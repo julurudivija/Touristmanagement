1d5e726496b3c2cb06750834af72345b651a4814
divija <divija.juloori@gmail.com>
2019-03-07 12:44:31 +0530

    hello hi iam divija

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD4rjSqrgABIhIk-Z6g1P2Bgr4bIXSiU8Q",
+    authDomain: "weather-233608.firebaseapp.com",
+    databaseURL: "https://weather-233608.firebaseio.com",
+    projectId: "weather-233608",
+    storageBucket: "weather-233608.appspot.com",
+    messagingSenderId: "171783249383"
+  }
+};
